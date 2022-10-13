65ba4c2dd6e349682c96e7b0215240944b39ffe6
Nikola Nekudova <nikolka.nekudova@seznam.cz>
2022-10-13 21:01:51 +0200

    feat(): constructors changed to classes

--- objects.js
+++ objects.js
@@ -17,11 +17,20 @@ const authorInput = document.getElementById("book-author");
 const pagesInput = document.getElementById("book-pages");
 const readInput = document.getElementById("book-read");
 
-function Book(title, author, pages, read) {
+/* function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+} */
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 renderHTML();
